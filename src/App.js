5dbf914695d31ed5a98a0ff787058cb567722d3b
tobbethun <tobbethun@gmail.com>
2020-10-11 00:02:28 +0200

    removed import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import GlobalFonts from './fonts/Fonts';
 import styled from 'styled-components/macro';
 import {DigitalBox} from "./common-style";
