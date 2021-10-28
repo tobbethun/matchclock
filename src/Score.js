22a1aa8211324d3792ea0dfd24f286a5af7b3fcb
tobithun <tobias.thun@tele2.com>
2021-10-28 09:36:15 +0200

    Remove abillity to set minusscore

--- src/Score.js
+++ src/Score.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
-import styled from 'styled-components/macro';
-import { DigitalBox } from './common-style';
-import { padWidthZero } from './utils';
+import React, { useState } from "react";
+import styled from "styled-components/macro";
+import { DigitalBox } from "./common-style";
+import { padWidthZero } from "./utils";
 
 const ScoreBox = styled(DigitalBox)`
-    font-size: 80px;
-    width: 88px;
+  font-size: 80px;
+  width: 88px;
 `;
 
 const EditScore = styled.div`
-    font-size: 32px;
-    display: flex;
-    justify-content: space-between;
-    span {
-        padding: 5px;
-        cursor: pointer;
-    }
+  font-size: 32px;
+  display: flex;
+  justify-content: space-between;
+  span {
+    padding: 5px;
+    cursor: pointer;
+  }
 `;
 
 const Score = () => {
-    const [score, setScore] = useState(0);
-    return (
-        <>
-            <ScoreBox onClick={() => setScore(score + 1)}>{padWidthZero(score)}</ScoreBox>
-            <EditScore>
-                <span onClick={() => setScore(score + 1)}>+</span>
-                <span onClick={() => setScore(score - 1)}>-</span>
-            </EditScore>
-        </>
-    );
+  const [score, setScore] = useState(0);
+
+  const reduceScore = () => {
+    if (score > 0) {
+      setScore(score - 1);
+    }
+  };
+
+  return (
+    <>
+      <ScoreBox onClick={() => setScore(score + 1)}>
+        {padWidthZero(score)}
+      </ScoreBox>
+      <EditScore>
+        <span onClick={() => setScore(score + 1)}>+</span>
+        <span onClick={reduceScore}>-</span>
+      </EditScore>
+    </>
+  );
 };
 
 export default Score;
